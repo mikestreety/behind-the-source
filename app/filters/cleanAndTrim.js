9abca2736bb102306b9c11bc0886a9744d4433ba
Mike Street <mikestreety@gmail.com>
2020-10-11 12:54:11 +0100

    Chore: Update whitespace

--- app/filters/cleanAndTrim.js
+++ app/filters/cleanAndTrim.js
@@ -1,8 +1,8 @@
 module.exports = function (text) {
-    text = text.replace(/&nbsp;/g, ' ');
-    text = text.replace(/(<([^>]+)>)/gi, "");
-    text = text.split(' ');
-    text = text.slice(0, 50);
-    text = text.join(' ');
-    return text;
+	text = text.replace(/&nbsp;/g, ' ');
+	text = text.replace(/(<([^>]+)>)/gi, "");
+	text = text.split(' ');
+	text = text.slice(0, 50);
+	text = text.join(' ');
+	return text;
 };
